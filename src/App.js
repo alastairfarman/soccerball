bfc20fa3a66469d464d30a0b4719b5bd203b2100
alastairfarman <106626671+alastairfarman@users.noreply.github.com>
2023-01-02 17:05:22 +0000

    some physics working. floor rotates.

--- src/App.js
+++ src/App.js
@@ -18,18 +18,39 @@ function App() {
 
     // gyroscope
 
-    function getAccel() {
-      DeviceMotionEvent.requestPermission().then((response) => {
-        if (response === "granted") {
-          console.log("acc permission granted");
+    function startMotionDetection() {
+      // feature detect
+      if (typeof DeviceMotionEvent.requestPermission === "function") {
+        DeviceMotionEvent.requestPermission()
+          .then((permissionState) => {
+            if (permissionState === "granted") {
+              window.addEventListener("devicemotion", () => {
+                detectMotion();
+              });
+            }
+          })
+          .catch(console.error);
+      } else {
+        detectMotion();
+      }
+    }
+    startMotionDetection();
 
-          window.addEventListener("devicemotion", (event) => {
-            console.log(event);
-          });
-          window.addEventListener("deviceorientation", (event) => {
-            console.log(event);
-          });
-        }
+    let deviceYRotation = 0;
+    let deviceXRotation = 0;
+
+    function detectMotion() {
+      // orientation in deg
+      window.addEventListener("deviceorientation", (event) => {
+        deviceYRotation = event.beta;
+        deviceXRotation = event.gamma;
+        // groundBody.quaternion.setFromEuler(
+        //   (deviceYRotation * Math.PI) / 180,
+        //   (deviceXRotation * Math.PI) / 180,
+        //   0
+        // );
+        console.log(group.rotation);
+        group.rotation.x = 0.3;
       });
     }
 
@@ -52,6 +73,9 @@ function App() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 0.5;
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
     document.getElementById("root").appendChild(renderer.domElement);
 
@@ -66,16 +90,58 @@ function App() {
       shape: new CANNON.Plane(),
     });
 
+    const ceilingBody = new CANNON.Body({
+      type: CANNON.Body.STATIC,
+      shape: new CANNON.Plane(),
+    });
+
     const wallL = new CANNON.Body({
       type: CANNON.Body.STATIC,
       shape: new CANNON.Plane(),
     });
 
-    groundBody.quaternion.setFromEuler(0, -0.2, 0);
-    wallL.quaternion.setFromEuler(0, 1, 0);
+    const wallR = new CANNON.Body({
+      type: CANNON.Body.STATIC,
+      shape: new CANNON.Plane(),
+    });
+
+    const wallT = new CANNON.Body({
+      type: CANNON.Body.STATIC,
+      shape: new CANNON.Plane(),
+    });
+
+    const wallB = new CANNON.Body({
+      type: CANNON.Body.STATIC,
+      shape: new CANNON.Plane(),
+    });
+
+    ceilingBody.position.set(0, 0, ballSize + 100);
+    ceilingBody.quaternion.setFromEuler(1.5708 * 2, 0, 0);
+
+    wallL.quaternion.setFromEuler(0, 1.5708, 0);
     wallL.position.set((arenaWidth / 2) * -1, 0, 0);
+
+    wallR.quaternion.setFromEuler(0, -1.5708, 0);
+    wallR.position.set(arenaWidth / 2, 0, 0);
+
+    wallT.quaternion.setFromEuler(1.5708, 0, 0);
+    wallT.position.set(0, arenaHeight / 2, 0);
+
+    wallB.quaternion.setFromEuler(-1.5708, 0, 0);
+    wallB.position.set(0, (arenaHeight / 2) * -1, 0);
+
     physicsWorld.addBody(groundBody);
     physicsWorld.addBody(wallL);
+    physicsWorld.addBody(wallR);
+    physicsWorld.addBody(wallT);
+    physicsWorld.addBody(wallB);
+    physicsWorld.addBody(ceilingBody);
+
+    const group = new THREE.Group();
+    group.add(groundBody);
+    group.add(wallL);
+
+    scene.add(group);
 
     const sphereBody = new CANNON.Body({
       mass: 5,
@@ -86,6 +152,16 @@ function App() {
 
     //light
 
+    const Dlight = new THREE.DirectionalLight(0xffffff, 1);
+    Dlight.position.set(-100, 120, 300);
+    Dlight.castShadow = true;
+    Dlight.shadow.camera.top = 200;
+    Dlight.shadow.camera.bottom = -200;
+    Dlight.shadow.camera.right = 200;
+    Dlight.shadow.camera.left = -200;
+    Dlight.shadow.mapSize.set(4096, 4096);
+    scene.add(Dlight);
+
     const hdrTextureURL = new URL(
       "../public/studio_small_03_4k.hdr",
       import.meta.url
@@ -95,6 +171,8 @@ function App() {
       texture.mapping = THREE.EquirectangularReflectionMapping;
       scene.environment = texture;
     });
+
+    //temp camera
 
     const orbit = new OrbitControls(camera, renderer.domElement);
 
@@ -119,17 +197,16 @@ function App() {
     const sphereGeo = new THREE.SphereGeometry(ballSize, 128, 128);
     const sphereMaterial = new THREE.MeshStandardMaterial({
       color: "#006f83",
-      roughness: "0.3",
+      roughness: "1",
     });
     const sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
-    // scene.add(sphere);
-
-    //arena
+    sphere.castShadow = true;
+    scene.add(sphere);
 
     const arenaGeo = new THREE.PlaneGeometry(arenaWidth, arenaHeight);
     const arenaMaterial = new THREE.MeshStandardMaterial({
-      color: "#ffffff",
-      roughness: "0.3",
+      color: "#AAAAAA",
+      roughness: "0.5",
     });
     const arenaMaterialWall = new THREE.MeshStandardMaterial({
       color: "#ffffff",
@@ -137,32 +214,33 @@ function App() {
     });
 
     const arenaFloor = new THREE.Mesh(arenaGeo, arenaMaterial);
+    arenaFloor.receiveShadow = true;
     const arenaWallTop = new THREE.Mesh(arenaGeo, arenaMaterialWall);
     const arenaWallBottom = new THREE.Mesh(arenaGeo, arenaMaterialWall);
     const arenaWallLeft = new THREE.Mesh(arenaGeo, arenaMaterialWall);
     const arenaWallRight = new THREE.Mesh(arenaGeo, arenaMaterialWall);
 
     scene.add(arenaFloor);
-    // scene.add(arenaWallTop);
-    // scene.add(arenaWallBottom);
-    // scene.add(arenaWallLeft);
-    // scene.add(arenaWallRight);
+    scene.add(arenaWallTop);
+    scene.add(arenaWallBottom);
+    scene.add(arenaWallLeft);
+    scene.add(arenaWallRight);
 
-    arenaWallTop.position.y = arenaHeight / 2;
-    arenaWallTop.position.z = arenaHeight / 2;
-    arenaWallTop.rotation.x = 1.5708;
+    // arenaWallTop.position.y = arenaHeight / 2;
+    // arenaWallTop.position.z = arenaHeight / 2;
+    // arenaWallTop.rotation.x = 1.5708;
 
-    arenaWallBottom.position.y = (arenaHeight / 2) * -1;
-    arenaWallBottom.position.z = (arenaHeight / 2) * 1;
-    arenaWallBottom.rotation.x = 1.5708 * -1;
+    // arenaWallBottom.position.y = (arenaHeight / 2) * -1;
+    // arenaWallBottom.position.z = (arenaHeight / 2) * 1;
+    // arenaWallBottom.rotation.x = 1.5708 * -1;
 
-    arenaWallLeft.position.x = (arenaWidth / 2) * -1;
-    arenaWallLeft.position.z = arenaWidth / 2;
-    arenaWallLeft.rotation.y = 1.5708;
+    // arenaWallLeft.position.x = (arenaWidth / 2) * -1;
+    // arenaWallLeft.position.z = arenaWidth / 2;
+    // arenaWallLeft.rotation.y = 1.5708;
 
-    arenaWallRight.position.x = arenaWidth / 2;
-    arenaWallRight.position.z = arenaWidth / 2;
-    arenaWallRight.rotation.y = 1.5708 * -1;
+    // arenaWallRight.position.x = arenaWidth / 2;
+    // arenaWallRight.position.z = arenaWidth / 2;
+    // arenaWallRight.rotation.y = 1.5708 * -1;
 
     //animate
 
@@ -178,6 +256,15 @@ function App() {
       sphere.position.copy(sphereBody.position);
       sphere.quaternion.copy(sphereBody.quaternion);
       arenaFloor.quaternion.copy(groundBody.quaternion);
+      arenaWallLeft.position.copy(wallL.position);
+      arenaWallLeft.quaternion.copy(wallL.quaternion);
+      arenaWallRight.position.copy(wallR.position);
+      arenaWallRight.quaternion.copy(wallR.quaternion);
+      arenaWallTop.position.copy(wallT.position);
+      arenaWallTop.quaternion.copy(wallT.quaternion);
+      arenaWallBottom.position.copy(wallB.position);
+      arenaWallBottom.quaternion.copy(wallB.quaternion);
+
       window.requestAnimationFrame(animate);
     };
     animate();
